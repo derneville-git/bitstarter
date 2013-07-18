35d9024c98586ee2a85a200a02dc69a5ece50ae0
Francois d'Erneville <derneville@gmail.com>
2013-07-18 22:39:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require('fs');
+    var htmltext = fs.readFileSync('index.html');
+    response.send(htmltext.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
